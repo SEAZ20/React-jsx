bb28ea0b03b5c57ebc5ef11150f8e021ad7a5232
SEAZ20 <seaz199820@gmail.com>
2021-11-13 15:58:38 -0500

    13/11/2021

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import faker from 'faker';
+import CommentDetails from './CommentDetails';
 
 const App = ()=>{
   return (
-    <div>
-      <label className="name" htmlFor="name">Ingrese su nombre: </label>
-      <input id="name" type="text"/>
-      <button style={{backgroundColor: "blue", color: "white"}}>
-        Submit
-      </button>
-
+    <div className="ui container comments">
+      <CommentDetails author="Silvio" timeAgo="Todat at 8:00pm" content="Hola" avatar={faker.image.avatar()}/>
+      <CommentDetails author="Enrique" timeAgo="Todat at 10:00pm" content="Nice blog post!" avatar={faker.image.avatar()} />
+      <CommentDetails author="Marina" timeAgo="Yesterday at 6:00pm" content="Hola" avatar={faker.image.avatar()}/>
+      <CommentDetails author="Jose" timeAgo="Todat at 9:00pm" content="Hola" avatar={faker.image.avatar()}/>
     </div>
   );
 }
